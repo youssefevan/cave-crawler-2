8aa552f98bc33e8ff6cd7454cb2e7a0e542de2b5
Evan <eyoussef128@gmail.com>
2023-10-30 16:41:40 -0400

    Main menu button functions
    - added base options menu

---
+++ CaveCrawler2/scripts/gui/menus/menu_main.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@export var level_select_scene : PackedScene
+@export var custom_level_interface_scene : PackedScene
+@export var options_scene : PackedScene
+
+func _on_play_pressed():
+	var ls = level_select_scene.instantiate()
+	get_tree().get_root().add_child(ls)
+	call_deferred("queue_free")
+
+func _on_editor_pressed():
+	var cli = custom_level_interface_scene.instantiate()
+	get_tree().get_root().add_child(cli)
+	call_deferred("queue_free")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_options_pressed():
+	var o = options_scene.instantiate()
+	get_tree().get_root().add_child(o)
+	visible = false
