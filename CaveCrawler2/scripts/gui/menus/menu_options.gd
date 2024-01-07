733a90942d381db1fef6ede5db9c9f5e9e77e565
Evan <eyoussef128@gmail.com>
2024-01-07 01:30:35 -0500

    Toggle fullscreen functionality

--- CaveCrawler2/scripts/gui/menus/menu_options.gd
+++ CaveCrawler2/scripts/gui/menus/menu_options.gd
@@ -2,3 +2,9 @@ extends Control
 
 func _on_back_pressed():
 	call_deferred("queue_free")
+
+func _on_toggle_fullscreen_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
