30427817d995fcc2b4fda49a9d363e7f3980e826
Evan <eyoussef128@gmail.com>
2024-02-12 20:32:34 -0500

    level 1-4 progress, bat speed change

--- CaveCrawler2/scripts/enemies/bat/bat.gd
+++ CaveCrawler2/scripts/enemies/bat/bat.gd
@@ -1,7 +1,7 @@
 extends Enemy
 class_name Bat
 
-var speed := 33.0
+var speed := 50.0
 var movement_direction : Vector2
 
 @onready var animator = $Animator
--- CaveCrawler2/scripts/enemies/bat/fly.gd
+++ CaveCrawler2/scripts/enemies/bat/fly.gd
@@ -4,8 +4,7 @@ func enter():
 	super.enter()
 
 func physics_update(delta):
-	
-	entity.movement_direction = (entity.player.global_position - entity.global_position).normalized()
+	entity.movement_direction = (entity.player.global_position - (entity.global_position + Vector2(0, 8))).normalized()
 	
 	entity.velocity.x = lerpf(
 		entity.velocity.x, entity.speed * entity.speed_modifier * entity.movement_direction.x, 5 * delta
