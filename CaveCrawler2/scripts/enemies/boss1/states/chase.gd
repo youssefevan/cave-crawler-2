ccb5a1e91dd066635110a0f77d264d3e359968b5
Evan <evanyoussef8@gmail.com>
2024-05-04 21:32:06 -0400

    changes to chase state

--- CaveCrawler2/scripts/enemies/boss1/states/chase.gd
+++ CaveCrawler2/scripts/enemies/boss1/states/chase.gd
@@ -5,12 +5,12 @@ var end := false
 func enter():
 	super.enter()
 	end = false
-	await get_tree().create_timer(5).timeout
+	await get_tree().create_timer(4).timeout
 	end = true
 
 func physics_update(delta):
 	entity.movement_direction = (entity.player.global_position - (entity.global_position + Vector2(0, 8))).normalized()
-	entity.velocity = lerp(entity.velocity, entity.speed * entity.speed_modifier * 0.3 * entity.movement_direction, 5 * delta)
+	entity.velocity = lerp(entity.velocity, entity.speed * entity.speed_modifier * .6 * entity.movement_direction, 5 * delta)
 	
 	if end == true:
 		return entity.idle
