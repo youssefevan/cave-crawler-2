5cb53342495fd951d37194bbaf494a9b9342a3cc
Evan <evanyoussef8@gmail.com>
2024-05-23 21:29:22 -0400

    Sync

--- CaveCrawler2/scripts/global.gd
+++ CaveCrawler2/scripts/global.gd
@@ -2,13 +2,13 @@ extends Node
 
 var save_path = "user://save_data.save"
 
-var main_menu_scene = preload("res://scenes/gui/menus/menu_main.tscn")
-var options_scene = preload("res://scenes/gui/menus/menu_options.tscn")
-var level_select_scene = preload("res://scenes/gui/menus/menu_level_select.tscn")
-var level_editor_scene = preload("res://scenes/level_editor/level_editor.tscn")
-var custom_level_loader_scene = preload("res://scenes/level_editor/custom_level_loader.tscn")
+var main_menu_scene = load("res://scenes/gui/menus/menu_main.tscn")
+var options_scene = load("res://scenes/gui/menus/menu_options.tscn")
+var level_select_scene = load("res://scenes/gui/menus/menu_level_select.tscn")
+var level_editor_scene = load("res://scenes/level_editor/level_editor.tscn")
+var custom_level_loader_scene = load("res://scenes/level_editor/custom_level_loader.tscn")
 var custom_level_interface_scene = load("res://scenes/gui/menu_custom_levels.tscn")
-var pause_scene = preload("res://scenes/gui/menus/menu_pause.tscn")
+var pause_scene = load("res://scenes/gui/menus/menu_pause.tscn")
 var player_scene = load("res://scenes/player.tscn")
 
 var player_cell_type = Vector2i(0, 4)
