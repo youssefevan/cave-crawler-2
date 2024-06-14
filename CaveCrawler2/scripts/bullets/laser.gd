42ec68584d22f4cdc4b7a081bfc999cc754c49fd
Evan <evanyoussef8@gmail.com>
2024-06-13 21:32:24 -0400

    work on boss 3

--- CaveCrawler2/scripts/bullets/laser.gd
+++ CaveCrawler2/scripts/bullets/laser.gd
@@ -1,1 +1,8 @@
 extends Area2D
+
+func _ready():
+	$Animator.play("Laser")
+
+
+func _on_animator_animation_finished(anim_name):
+	call_deferred("queue_free")
--- CaveCrawler2/scripts/enemies/boss3/boss_3.gd
+++ CaveCrawler2/scripts/enemies/boss3/boss_3.gd
@@ -1,11 +1,25 @@
 extends Enemy
 class_name Boss3
 
+@onready var states = $StateManager
 @onready var idle = $StateManager/Idle
+
+@onready var muzzle = $Muzzle
 
 func _ready():
 	super._ready()
-	$Animator.play("Idle")
+	
+	states.init(self)
 
 func _physics_process(delta):
-	pass
+	super._physics_process(delta)
+	states.physics_update(delta)
+	apply_gravity(delta)
+	
+	muzzle.look_at(player.global_position + Vector2(0, 4))
+	
+	move_and_slide()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * 0.1 * delta
