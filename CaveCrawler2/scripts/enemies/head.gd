f43281a96d1e55b4bace09338328d9b591efceff
Evan <evanyoussef8@gmail.com>
2024-06-19 19:24:11 -0400

    changes to boss 3 head

--- CaveCrawler2/scripts/enemies/head.gd
+++ CaveCrawler2/scripts/enemies/head.gd
@@ -1,10 +1,14 @@
 extends Enemy
 
-var speed := 100.0
+var speed := 60.0
+
+func _ready():
+	super._ready()
+	$Animator.play("Spawn")
 
 func _physics_process(delta):
 	var movement_direction = (player.global_position - (global_position + Vector2(0, 8))).normalized()
-	velocity = lerp(velocity, speed * .6 * movement_direction, 5 * delta)
+	velocity = lerp(velocity, speed * movement_direction, 5 * delta)
 	move_and_slide()
 	
 	
