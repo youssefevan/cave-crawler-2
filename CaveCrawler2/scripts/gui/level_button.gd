8849fc31e776681f44ce835b6ff740fe0bd68bb1
Evan <evanyoussef8@gmail.com>
2024-08-29 14:51:51 -0400

    Added world 1 music

--- CaveCrawler2/scripts/gui/level_button.gd
+++ CaveCrawler2/scripts/gui/level_button.gd
@@ -6,6 +6,7 @@ var level_id : int
 
 signal level_selected(level)
 
+var music_w1 = preload("res://audio/music/world1.ogg")
 var music_w2 = preload("res://audio/music/world2.ogg")
 
 func _ready():
@@ -32,7 +33,9 @@ func _ready():
 func _on_pressed():
 	super._on_pressed()
 	var world_number = str(name)[0]
-	if world_number == "2":
+	if world_number == "1":
+		AudioHandler.play_music(music_w1, false)
+	elif world_number == "2":
 		AudioHandler.play_music(music_w2, false)
 	
 	emit_signal("level_selected", level)
