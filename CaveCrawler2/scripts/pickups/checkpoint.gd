21aa9e7fd2f03aba0ef21c3136a0fcbd8d0ef28b
Evan <evanyoussef8@gmail.com>
2024-10-08 16:10:24 -0400

    fixed health re-pickup, adjusted level editor spawn

--- CaveCrawler2/scripts/pickups/checkpoint.gd
+++ CaveCrawler2/scripts/pickups/checkpoint.gd
@@ -9,18 +9,20 @@ var particles = preload("res://scenes/particles/health_picked.tscn")
 var sfx = preload("res://audio/sfx/checkpoint.ogg")
 
 func picked(body) -> void:
-	super.picked(body)
-	$Sprite.frame = 1
-	body.health = body.max_health
-	
-	var p = particles.instantiate()
-	add_child(p)
-	p.position = Vector2(0, -16)
-	
-	AudioHandler.play_sfx(sfx)
-	
-	Global.checkpoint_passed = true
-	Global.checkpoint_position = global_position - spawn_offset
+	#print(Global.checkpoint_position)
+	if Global.checkpoint_position != global_position - spawn_offset:
+		super.picked(body)
+		$Sprite.frame = 1
+		body.health = body.max_health
+		
+		var p = particles.instantiate()
+		add_child(p)
+		p.position = Vector2(0, -16)
+		
+		AudioHandler.play_sfx(sfx)
+		
+		Global.checkpoint_passed = true
+		Global.checkpoint_position = global_position - spawn_offset
 
 func get_level_editor_offset() -> Vector2:
 	return level_editor_offset
--- CaveCrawler2/scripts/pickups/health.gd
+++ CaveCrawler2/scripts/pickups/health.gd
@@ -3,19 +3,24 @@ extends Pickup
 var health_particles = load("res://scenes/particles/health_picked.tscn")
 var sfx_pickup = preload("res://audio/sfx/health_pickup.ogg")
 
+var picked_up := false
+
+@export var spawn_offset : Vector2
+
 func _ready():
 	$Animator.play("Bob")
 
 func picked(body):
-	super.picked(body)
-	call_deferred("disable")
-	if body.health != body.max_health:
-		body.health += 1
-		picked_anim()
-		AudioHandler.play_sfx(sfx_pickup)
+	if !picked_up:
+		if body.health != body.max_health:
+			super.picked(body)
+			body.health += 1
+			picked_up = true
+			picked_anim()
+			AudioHandler.play_sfx(sfx_pickup)
 
-func disable():
-	$Area/Collider.disabled = true
+#func disable():
+	#$Area/Collider.disabled = true
 
 func picked_anim():
 	if OptionsHandler.particles_enabled == true:
@@ -28,3 +33,6 @@ func picked_anim():
 func _on_animator_animation_finished(anim_name):
 	if anim_name == "Picked":
 		call_deferred("queue_free")
+
+func get_level_editor_offset() -> Vector2:
+	return spawn_offset
